318d647db5e1da1fbf6e4d15d00a928e6355145e
Guido98 <guido.rc98@gmail.com>
2022-02-03 18:40:26 -0300

    Agrego código HTML para mostrar el historial de compras

--- Frontend/src/ProductoEnHistorial.jsx
+++ Frontend/src/ProductoEnHistorial.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-function Producto(item) {
+const Producto = ({ item }) => {
   return (
     <div className="col-lg-4">
       <div className="card m-2">
         <div className="card-body">
           <div className="text-muted">#{item.id}</div>
           <h5 className="pt-5 border-top">{item.title}</h5>
-          <div>${item.total}</div>
-        </div>
-        <div className="card-footer">
-          <div className="float-left">
-            <span className="badge">{item.cantidad}</span>
-          </div>
+          {item.id !== "total" ? <div>Cantidad: {item.quantity}</div> : null}
+          {item.id !== "total" ? (
+            <div>Precio por unidad: ${item.total}</div>
+          ) : (
+            <div>${item.total}</div>
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Producto;
